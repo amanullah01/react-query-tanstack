11bb08ae1c8f10f70a183287ad9bf48ad7544b91
Amanullah Aman <amancse.aiub@gmail.com>
2023-08-31 17:53:57 +0600

    acting on mutatioin success and invalid query after sucesss

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import EventDetails from "./components/Events/EventDetails.jsx";
 import NewEvent from "./components/Events/NewEvent.jsx";
 import EditEvent from "./components/Events/EditEvent.jsx";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./components/util/http.js";
 
 const router = createBrowserRouter([
   {
@@ -38,8 +39,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
